f8792cbca349cf0591d068c981b02c1496602ea1
Khiem NV <nvkhiem2016@gmail.com>
2023-10-27 01:29:43 +0700

    feat(infra): add docker infra, and example event, message

--- apps/user-gateway/src/apps.controller.ts
+++ apps/user-gateway/src/apps.controller.ts
@@ -1,9 +1,39 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Inject, OnModuleInit } from '@nestjs/common';
 import { AppsService } from './apps.service';
+import { ClientKafka } from '@nestjs/microservices';
 
 @Controller()
-export class AppsController {
-  constructor(private readonly appsService: AppsService) {}
+export class AppsController implements OnModuleInit {
+  constructor(
+    private readonly appsService: AppsService,
+    @Inject('AUTH_SERVICE') private client: ClientKafka,
+  ) { }
+
+  onModuleInit() {
+    this.client.subscribeToResponseOf("auth.send");
+    this.client.connect()
+  }
+
+  @Get('auth/emit')
+  emit() {
+
+    try {
+      this.client.emit("auth.emit", { test: "emit" });
+    } catch (error) {
+      console.error("Error sending Kafka message:", error);
+    }
+  }
+
+  @Get('auth/send')
+  send() {
+    try {
+      this.client.send("auth.send", { test: "send" }).subscribe((data) => {
+        console.log("Received data:", data);
+      });
+    } catch (error) {
+      console.error("Error sending Kafka message:", error);
+    }
+  }
 
   @Get()
   getHello(): string {
--- apps/user-gateway/src/apps.module.ts
+++ apps/user-gateway/src/apps.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+
 import { AppsController } from './apps.controller';
 import { AppsService } from './apps.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'AUTH_SERVICE',
+        transport: Transport.KAFKA,
+        options: {
+          client: {
+            brokers: ['localhost:9092'],
+          },
+          consumer: {
+            groupId: 'auth-consumer'
+          }
+        }
+      },
+    ]),
+  ],
   controllers: [AppsController],
   providers: [AppsService],
 })
